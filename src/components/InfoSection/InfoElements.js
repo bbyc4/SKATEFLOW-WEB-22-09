7c8638d9a8ee4f149b31d19a3c705e40a26fbe4d
SkateFlow <skateflowG5@gmail.com>
2024-09-21 17:57:39 -0300

    skate2

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const InfoContainer = styled.div`
     color: #fff;
-    background: ${({ lightBg }) => (lightBg ? '#f9f9f9' : '#000')};
+    background: ${({ lightBg }) => (lightBg ? '#f9f9f9' : '#001426')};
 
     @media screen and (max-width: 768px) {
         padding: 100px 0;
