2c2ce249c8a1370e87895f0284a9884a4a2189c5
vinicius7og <vneditsss@gmail.com>
2024-10-16 11:58:54 -0300

    SKATEFLOW ATUALIZACAO VINICIUS 16/10

--- src/components/Services/ServicesElements.js
+++ src/components/Services/ServicesElements.js
@@ -37,21 +37,22 @@ export const ServicesWrapper = styled.div`
 `
 
 export const ServicesCard = styled.div`
-    background: #fff;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    border-radius: 10px;
-    max-height: 340px;
-    padding: 30px;
-    box-shadow: 0 1px 3px rgba(0,0,0,0.2);
-    transition: all 0.2s ease-in-out;
+background: #fff;
+display: flex;
+flex-direction: column;
+justify-content: space-between; /* Altera de flex-start para space-between */
+align-items: center;
+border-radius: 10px;
+height: 340px; /* Altura fixa para os cards */
+padding: 30px;
+box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+transition: all 0.2s ease-in-out;
 
-    &:hover {
-        transform: scale(1.02);
-        transition: all 0.2s ease-in-out;
-        cursor: pointer;
+&:hover {
+    transform: scale(1.02);
+    transition: all 0.2s ease-in-out;
+    cursor: pointer;
+
     }
 `
 
@@ -85,4 +86,8 @@ export const BtnWrap = styled.div`
     display: flex;
     justify-content: center;
     margin-top: 32px;
+    
+    
 `
+
+
